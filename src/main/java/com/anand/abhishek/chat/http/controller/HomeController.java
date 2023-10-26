d145f49fff30330a611b02b639f926769543c069
Abhishek Anand <anand.abhishek73@gmail.com>
2023-10-26 22:40:36 +0530

    added a throwable login path to controller

--- src/main/java/com/anand/abhishek/chat/http/controller/HomeController.java
+++ src/main/java/com/anand/abhishek/chat/http/controller/HomeController.java
@@ -3,8 +3,10 @@ package com.anand.abhishek.chat.http.controller;
 import com.anand.abhishek.chat.http.model.ApiResponse;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
@@ -20,5 +22,12 @@ public class HomeController {
     ) {
         log.debug("Got a request on home controller");
         return new ApiResponse("success");
+    }
+
+    @GetMapping(
+            path = "/login",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public void errorHandling(){
+        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED, "Login is not implemented in the project");
     }
 }
